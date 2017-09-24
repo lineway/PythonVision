2f0dd2de8f126682d66545f5514f63d10799cb44
张一铭 <zhangyiming07@163.com>
2017-09-24 23:40:52 +0800

    增加直方图均衡化函数

--- imtools.py
+++ imtools.py
@@ -21,3 +21,16 @@ def imresize(im, sz):
     '''
     pil_im = Image.fromarray(uint8(im))
     return array(pil_im.resize(sz))
+
+def histeq(im, nbr_bins=256):
+    '''
+    对一幅灰度图像进行直方图均衡化
+    '''
+    imhist, bins = histogram(im.flatten(), nbr_bins, normed=True)
+    # cumulative distribution function
+    cdf = imhist.cumsum()
+    # 归一化处理
+    cdf = 255 * cdf / cdf[-1]
+    # 使用累积分布函数的线性插值，计算新的像素值
+    im2 = interp(im.flatten(), bins[:-1], cdf)
+    return im2.reshap(im.shape), cdf
